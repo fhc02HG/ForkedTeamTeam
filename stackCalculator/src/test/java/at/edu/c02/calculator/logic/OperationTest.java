c47e470ce1cab1332657e15a5cac199c107d30a0
gerzic <gerzic@ucl10496.knapp.at>
2019-01-12 09:57:46 +0100

    Adding everything

--- stackCalculator/src/test/java/at/edu/c02/calculator/logic/OperationTest.java
+++ stackCalculator/src/test/java/at/edu/c02/calculator/logic/OperationTest.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 import at.edu.c02.calculator.CalculatorException;
 import at.edu.c02.calculator.MathOperation;
 import at.edu.c02.calculator.logic.operations.Add;
+import at.edu.c02.calculator.logic.operations.Div;
+import at.edu.c02.calculator.logic.operations.Mul;
+import at.edu.c02.calculator.logic.operations.Sub;
 
 public class OperationTest {
 
@@ -22,7 +25,47 @@ public class OperationTest {
 		//verify
 		assertEquals(3, result, 0.0001);
 	}
+	
+	@Test
+	public void testMul() throws CalculatorException {
+		
+		//setup
+		MathOperation mul = new Mul();
+		
+		//execute
+		double result = mul.calculate(2,2);
+		
+		//verify
+		assertEquals(4, result, 0.0001);
+		
+	}
 
+	@Test
+	public void testDiv() throws CalculatorException {
+		
+		//setup
+		MathOperation div = new Div();
+		
+		//execute
+		double result = div.calculate(10,5);
+		
+		//verify
+		assertEquals(2, result, 0.0001);
+		
+	}
 
 	
+	@Test
+	public void testSub() throws CalculatorException {
+		
+		//setup
+		MathOperation sub = new Sub();
+		
+		//execute
+		double result = sub.calculate(12,5);
+		
+		//verify
+		assertEquals(7, result, 0.0001);
+		
+	}
 }
